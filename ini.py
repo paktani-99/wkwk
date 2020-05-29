c6e2a3640c9a602cfdfe16d6f99876307d673bad
paktani-99 <66012458+paktani-99@users.noreply.github.com>
2020-05-29 20:38:59 +0700

    Add files via upload
    dsiodhsoi

---
+++ ini.py
@@ -0,0 +1,16 @@
+import csv
+
+data={}
+
+dt=open("dtpaket.csv","r")
+    
+per=dt.read()
+#for a in per :
+    #print (a)
+
+#print (a[3])
+data["J"]=per[4]:
+print (data)
+#print (per[0])
+
+#print(row[1]) 
